b81ff3ce574374a322cc00891c94b166b3ea4674
Michel du Croix <mlcroix@avans.nl>
2016-12-22 03:14:28 +0100

    PowerMode catching bees + return to base
    de impker zal nu bijen vangen en terug naar de basis gaan zodra hij er
    30 heeft gevangen.

--- SDLFramework/SDLFramework/PowerMode.cpp
+++ SDLFramework/SDLFramework/PowerMode.cpp
@@ -1,7 +1,17 @@
 #include "PowerMode.h"
+#include "Imker.h"
+#include "GoTOBase.h"
+#include <vector>
+#include "IGameObject.h"
+#include "GameController.h"
+#include "Vector2D.h"
+#include "Bee.h"
+#include "string"
+#include "Iostream"
 
 PowerMode::PowerMode(Imker * imker)
 {
+	_imker = imker;
 }
 
 PowerMode::~PowerMode()
@@ -10,10 +20,27 @@ PowerMode::~PowerMode()
 
 void PowerMode::checkState(GameController * controller)
 {
+	if (_imker->getChatchedBees().size() == 30) {
+		_imker->setBehavior(new GoToBase(_imker));
+	}
 }
 
 void PowerMode::update(GameController * controller)
 {
+	std::vector<IGameObject*> catchedBees;
+	Vector2D imkerPos;
+	imkerPos.setX(_imker->getX());
+	imkerPos.sety(_imker->getY());
+
+	for each (Bee* bee in controller->getGameobjecten())
+	{
+		if (imkerPos.dist(imkerPos, bee->getPosition()) <= _imker->getCatchRadius() && !bee->isCathced()) {
+			bee->setCathced(true);
+			_imker->addcatchedBee(bee);
+			std::cout << "Catched a bee" << std::endl;
+			std::cout << "Bee's: " << std::to_string(_imker->getChatchedBees().size()) << std::endl;
+		}
+	}
 }
 
 void PowerMode::Move(GameController * controller)
